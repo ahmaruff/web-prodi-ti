4e962ffd4358b0689b08f0080f3d215d099c4134
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-23 14:12:47 +0700

    article resources

---
+++ app/Http/Controllers/Admin/ArtikelController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreArtikelRequest;
+use App\Http\Requests\UpdateArtikelRequest;
+use App\Models\Artikel;
+
+class ArtikelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = [];
+        return view('admin.artikel.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.artikel.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreArtikelRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Artikel $artikel)
+    {
+        $data = [];
+        return view('admin.artikel.show', $data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Artikel $artikel)
+    {
+        $data = [];
+        return view('admin.artikel.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateArtikelRequest $request, Artikel $artikel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Artikel $artikel)
+    {
+        //
+    }
+}
