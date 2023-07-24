2accfdada1f23ae4c9bb427098ccb199d7927890
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-24 09:35:24 +0700

    fix artikel controller. missing use controller

--- app/Http/Controllers/Admin/ArtikelController.php
+++ app/Http/Controllers/Admin/ArtikelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreArtikelRequest;
 use App\Http\Requests\UpdateArtikelRequest;
 use App\Models\Artikel;
