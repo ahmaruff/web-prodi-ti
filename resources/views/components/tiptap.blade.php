5233ca03400de2fd0297f8883e2df37f220c7faa
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-25 12:25:05 +0700

    add blade coolicon and insert to tiptap menu

--- resources/views/components/tiptap.blade.php
+++ resources/views/components/tiptap.blade.php
@@ -6,73 +6,73 @@
         @click="toggleHeading(1)"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('heading', { level: 1 }, updatedAt) }"
         >
-        H1
+            <x-coolicon-heading-h1 class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg "
         @click="toggleHeading(2)"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('heading', { level: 2 }, updatedAt) }"
         >
-        H2
+            <x-coolicon-heading-h2 class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleHeading(3)"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('heading', { level: 3 }, updatedAt) }"
         >
-        H3
+            <x-coolicon-heading-h3 class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleHeading(4)"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('heading', { level: 3 }, updatedAt) }"
         >
-        H4
+            <x-coolicon-heading-h4 class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleBold()"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('bold', updatedAt) }"
         >
-        bold
+            <x-coolicon-bold class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleItalic()"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('italic', updatedAt) }"
         >
-        italic
+            <x-coolicon-italic class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleStrike()"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('strike', updatedAt) }"
         >
-        strike
+            <x-coolicon-strikethrough class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleBlockquote()"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('blockquote', updatedAt) }"
         >
-        quotes
+            <x-coolicon-double-quotes-r class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleCode()"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('code', updatedAt) }"
         >
-        inline code
+            <x-coolicon-code class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleCodeBlock()"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('codeBlock', updatedAt) }"
         >
-        code block
+            <x-coolicon-window-code-block class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleBulletList()"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('bulletList', updatedAt) }"
         >
-        bullet list
+            <x-coolicon-list-unordered class="w-5" />
         </button>
         <button class="px-2 py-1 border-2 border-gray-700 rounded-lg"
         @click="toggleOrderedList()"
         :class="{ 'bg-gray-700 text-gray-50' : isActive('orderedList', updatedAt) }"
         >
-        ordered list
+            <x-coolicon-list-ordered class="w-5" />
         </button>
       </div>
     <div x-ref="editor"></div>
