41be94caaed987b555c99fd46cf1a210638164be
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-25 20:51:32 +0700

    change HOME route pointing to /admin/dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->prefix('admin')->group(function () {
+    Route::view('/', 'dashboard');
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
