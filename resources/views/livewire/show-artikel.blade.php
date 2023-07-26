dd86a5bb107f4545f0ef1d4b86623edded251972
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-26 15:47:08 +0700

    install and publish config vendor livewire-datatables

--- resources/views/livewire/show-artikel.blade.php
+++ resources/views/livewire/show-artikel.blade.php
@@ -1,3 +1,13 @@
 <div>
-    @dump($artikels)
+    {{-- @dump($artikels) --}}
+    @foreach ($artikels as $artikel)
+    <h1>{{ $artikel->title }}</h1>
+    <p>{{ $artikel->id }}</p>
+    <p>{{ $artikel->category }}</p>
+    <p>{{ $artikel->is_publised }}</p>
+
+    <a href="{{$artikel->id}}">show</a>
+    <a href="edit/{{$artikel->id}}">Edit</a>
+
+    @endforeach
 </div>
