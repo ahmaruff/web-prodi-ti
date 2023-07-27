ec724396e7ec6b276f6ce52ed2940623843f79ad
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-27 21:45:42 +0700

    add profil prodi routes

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,11 @@ Route::middleware('auth')->prefix('admin')->group(function () {
 
 
     Route::resource('artikel','\App\Http\Controllers\Admin\ArtikelController');
+
+    // PROFIL
+    Route::prefix('profil')->group(function() {
+        Route::get('/',[\App\Http\Controllers\Admin\ProfilProdiController::class, 'index'])->name('admin.profil-prodi.index');
+    });
     // Route::get('artikel', [\App\Http\Controllers\Admin\ArtikelController::class, 'index'])->name('admin.artikel.index');
     // Route::get('editor', [\App\Http\Controllers\Admin\ArtikelController::class, 'create'])->name('admin.artikel.create');
 });
