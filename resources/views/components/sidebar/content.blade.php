a4a64798bb37365d01d27f620baab9cd9b21cebc
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-27 21:46:32 +0700

    change sidebar menu list for profil

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -10,34 +10,9 @@
         :isActive="request()->routeIs('dashboard')"
     >
         <x-slot name="icon">
-            <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+            <x-heroicon-o-home class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
     </x-sidebar.link>
-
-    <x-sidebar.dropdown
-        title="Buttons"
-        :active="Str::startsWith(request()->route()->uri(), 'buttons')"
-    >
-        <x-slot name="icon">
-            <x-heroicon-o-view-grid class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
-        </x-slot>
-
-        <x-sidebar.sublink
-            title="Text button"
-            href="{{ route('buttons.text') }}"
-            :active="request()->routeIs('buttons.text')"
-        />
-        <x-sidebar.sublink
-            title="Icon button"
-            href="{{ route('buttons.icon') }}"
-            :active="request()->routeIs('buttons.icon')"
-        />
-        <x-sidebar.sublink
-            title="Text with icon"
-            href="{{ route('buttons.text-icon') }}"
-            :active="request()->routeIs('buttons.text-icon')"
-        />
-    </x-sidebar.dropdown>
 
     <div
         x-transition
@@ -51,9 +26,50 @@
     title="Artikel"
     href="{{route('artikel.index')}}"
     :isActive="request()->routeIs('artikel.*')">
-        @slot('icon') <x-coolicon-notebook class="w-6 h-6"/>@endslot
+        @slot('icon') <x-heroicon-o-document-text class="flex-shrink-0 w-6 h-6"/>@endslot
         Artikel
     </x-sidebar>
+
+    <x-sidebar.dropdown
+        title="Profil Prodi"
+        :active="Str::startsWith(request()->route()->uri(), 'admin/profil')"
+    >
+        <x-slot name="icon">
+            <x-heroicon-o-academic-cap class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+        </x-slot>
+
+        <x-sidebar.sublink
+            title="Visi Misi"
+            href="{{ route('admin.profil-prodi.index') }}"
+            :active="request()->routeIs('admin.profil-prodi.index')"
+        />
+        <x-sidebar.sublink
+            title="Profil Lulusan"
+            href="{{ route('buttons.icon') }}"
+            :active="request()->routeIs('buttons.icon')"
+        />
+
+        <x-sidebar.sublink
+            title="Akreditasi"
+            href="{{ route('buttons.text-icon') }}"
+            :active="request()->routeIs('buttons.text-icon')"
+        />
+        <x-sidebar.sublink
+            title="Struktur Organisasi"
+            href="{{ route('buttons.text-icon') }}"
+            :active="request()->routeIs('buttons.text-icon')"
+        />
+        <x-sidebar.sublink
+            title="Dosen"
+            href="{{ route('buttons.text-icon') }}"
+            :active="request()->routeIs('buttons.text-icon')"
+        />
+        <x-sidebar.sublink
+            title="Fasilitas"
+            href="{{ route('buttons.text-icon') }}"
+            :active="request()->routeIs('buttons.text-icon')"
+        />
+    </x-sidebar.dropdown>
 
     @php
         $links = array_fill(0, 20, '');
