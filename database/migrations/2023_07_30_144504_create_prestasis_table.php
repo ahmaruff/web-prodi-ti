2fb29be99e8bfd2bae618f67c4f33db0e19b9306
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-30 21:50:38 +0700

    create model and migration for prestasi mhs

---
+++ database/migrations/2023_07_30_144504_create_prestasis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prestasis', function (Blueprint $table) {
+            $table->id();
+            $table->string('juara');
+            $table->string('deskripsi');
+            $table->string('nama_kompetisi');
+            $table->enum('tingkatan',['kampus','kabupaten','provinsi','nasional','internasional']);
+            $table->string('penyelenggara')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prestasis');
+    }
+};
