710a322fca77910c81593fd8e1e4cb8fb4f9240b
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-30 20:23:09 +0700

    modify livewire datatables link attributs

--- resources/views/livewire/datatables/link.blade.php
+++ resources/views/livewire/datatables/link.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ $href }}" class="border-2 border-transparent hover:border-blue-500 hover:bg-blue-100 hover:shadow-lg text-blue-600 rounded-lg px-3 py-1">{{ $slot }}</a>
+<a href="{{ $href }}" class="text-blue-500 hover:text-blue-800" target="_blank" rel="noreferrer noopener">{{ $slot }}</a>
